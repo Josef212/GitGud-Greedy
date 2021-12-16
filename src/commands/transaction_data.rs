fe3cb3b4f854433dfe41148541669ca809a2e215
Josef212 <jcasanovasv@hotmail.com>
2021-12-16 23:16:35 +0100

    Get transactions data command

---
+++ src/commands/transaction_data.rs
@@ -0,0 +1,37 @@
+use clap::Parser;
+use log;
+
+use crate::commands::sub_cmd::{SubCmd, ask_parameter};
+use crate::models::Db;
+use crate::commons::Opts;
+use crate::models::transaction::Transaction;
+
+#[derive(Parser, Debug)]
+pub struct TransactionData {
+    #[clap(short, long)]
+    year: Option<u32>,
+    #[clap(short, long)]
+    month: Option<u32>,
+}
+
+impl SubCmd for TransactionData {
+    fn execute(&self, db: &Db, _opts: &Opts) {
+        let transactions = db.get_transaction_data(self.year, self.month).unwrap_or_else(|e| {
+            log::error!("Error getting transactions: {}", e);
+            std::process::exit(0);
+        });
+
+        // TODO: Format results
+        // TODO: Generate view model and view renderer
+
+        log::info!("Transactions =====================");
+        for t in &transactions {
+            let tag = db.get_tag_str(t.tag_id).unwrap_or(String::from("Unknown"));
+
+            log::info!(
+                "  [{:04}] Name: {} - Date: {} - Amount: {} - Tag: {}",
+                t._id, t.name, t.date, t.amount, tag
+            );
+        }
+    }
+}
--- src/models/transaction.rs
+++ src/models/transaction.rs
@@ -1,6 +1,8 @@
+use rusqlite::Row;
+
 #[derive(Debug)]
 pub struct Transaction {
-    _id: i32,
+    pub _id: i32,
     pub name: String,
     pub date: String,
     pub amount: f32,
@@ -15,6 +17,16 @@ impl Transaction {
             date: date.clone(), 
             amount, 
             tag_id
+        }
+    }
+
+    pub fn from_row(r: &Row) -> Transaction {
+        Transaction {
+            _id: r.get_unwrap(0),
+            name: r.get_unwrap(1),
+            date: r.get_unwrap(2),
+            amount: r.get_unwrap(3),
+            tag_id: r.get_unwrap(4),
         }
     }
 }

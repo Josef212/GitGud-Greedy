a0dcb874361b748d6e9fec95c672aad7370196da
Josef212 <jcasanovasv@hotmail.com>
2021-12-18 14:28:21 +0100

    Improved payrolls and transactions data

--- src/commands/transaction_data.rs
+++ src/commands/transaction_data.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use clap::Parser;
 use log;
 
@@ -23,13 +24,42 @@ impl SubCmd for TransactionData {
         // TODO: Format results
         // TODO: Generate view model and view renderer
 
+        let mut total_amount = 0.0;
+        let mut tags_count: HashMap<i32, u32> = HashMap::new();
+        let mut amount_per_tags: HashMap<i32, f32> = HashMap::new();
+
         log::info!("Transactions =====================");
         for t in &transactions {
+            
+            total_amount += t.amount;
+            *tags_count.entry(t.tag_id).or_insert(0) += 1;
+            *amount_per_tags.entry(t.tag_id).or_insert(0.0) += t.amount;
+            
             let tag = db.get_tag_str(t.tag_id).unwrap_or(String::from("Unknown"));
 
             log::info!(
                 "  [{:04}] Name: {} - Date: {} - Amount: {} - Tag: {}",
                 t._id, t.name, t.date, t.amount, tag
+            );
+        }
+        
+        let amount_avg: f32 = total_amount / (transactions.len() as f32);
+        
+        log::info!("======================================");
+        log::info!("Total spent: {}", total_amount);
+        log::info!("Total tags: {}", tags_count.len());
+        log::info!("Avg per tag: {}", amount_avg);
+        
+        for t in tags_count {
+            let tag_id = t.0;
+            let count: u32 = t.1;
+            let tag = db.get_tag_str(tag_id).unwrap_or(String::from("Unknown"));
+            let amount = amount_per_tags[&tag_id];
+            let avg: f32 = amount / (count as f32);
+            
+            log::info!(
+                "  [{}] Name: {} - A: {} - C: {} - Avg: {}",
+                tag_id, tag, amount, count, avg
             );
         }
     }
